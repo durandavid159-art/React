7b246afb355d03d060e77fbbe0cfcdf0b3fe4a2e
durandavid159-arte <durandavid159@gmail.com>
2026-05-26 22:01:27 -0500

    DP se crea la carpeta components y las sub carpetas Header, itemNavbar, logo, y Navbar para darle estructura al proyecto

--- first-app/src/App.jsx
+++ first-app/src/App.jsx
@@ -1,20 +1,10 @@
-
-import './App.css'
+import { Header } from './components/Header/Header' 
 
 export const App = () => {
 
   return (
     <>
-      <header>
-        <nav>
-          <ul>
-            <li><a href="">Inicio</a> </li>
-            <li><a href="">Sobre mi</a></li>
-            <li><a href="">Habilidades</a></li>
-            <li><a href="">Contacto</a></li>
-          </ul>
-        </nav>
-      </header>
+      <Header></Header>
 
       <main>
         <section>
---
+++ first-app/src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Header.css'
+
+export const Header = () => {
+    return(
+    <header>
+        <nav>
+            <ul>
+                <li><a href="">Inicio</a> </li>
+                <li><a href="">Sobre mi</a></li>
+                <li><a href="">Habilidades</a></li>
+                <li><a href="">Contacto</a></li>
+            </ul>
+        </nav>
+    </header>
+    )
+}
