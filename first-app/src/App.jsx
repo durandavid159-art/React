154f0ef44bf72193e91902cfb36ba360856652f5
durandavid159-arte <durandavid159@gmail.com>
2026-05-31 02:39:58 -0500

    DP se organiza el componente del footer para darle mejor estructura al proyecto

--- first-app/src/App.jsx
+++ first-app/src/App.jsx
@@ -1,5 +1,6 @@
 import { Header } from './components/Header/Header' 
 import {Main} from './components/Main/Main'
+import {Footer} from './components/Footer/Footer'
 
 export const App = () => {
 
@@ -7,13 +8,7 @@ export const App = () => {
     <>
       <Header></Header>
       <Main></Main>
-
-      <footer>
-      <h2>Contacto</h2>
-        <p>Correo: DavidPescador@gmail.com</p>
-        <p>GitHub: <a href="https://github.com/durandavid159-art"  target="_blank" rel="noreferrer">github.com/david</a></p>
-        <p>LinkedIn: <a href="https://www.linkedin.com/feed/"  target="_blank" rel="noreferrer">linkedin.com/in/david</a></p>
-      </footer>
+      <Footer></Footer>
     </>
   )
 }
---
+++ first-app/src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Footer.css'
+
+export const Footer = () =>(
+
+    <footer>
+        <h2>Contacto</h2>
+        <p>Correo: DavidPescador@gmail.com</p>
+        <p>GitHub: <a href="https://github.com/durandavid159-art"  target="_blank" rel="noreferrer">github.com/david</a></p>
+        <p>LinkedIn: <a href="https://www.linkedin.com/feed/"  target="_blank" rel="noreferrer">linkedin.com/in/david</a></p>
+    </footer>
+)
